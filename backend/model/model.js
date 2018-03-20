e6a0c8cb4780affd6e216baf7cabc3b5eace32dc
fullautomedia <taryn@fullautomedia.com>
2018-03-19 20:57:13 -0600

    update backend model

--- backend/model/model.js
+++ backend/model/model.js
@@ -4,15 +4,15 @@ module.exports = function() {
     var db = new sqlite3.Database(':memory:');
     
     db.serialize(function() {
-        db.run("CREATE TABLE lorem (info TEXT)");
+        db.run("CREATE TABLE order (info TEXT)");
         
-        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        var stmt = db.prepare("INSERT INTO order VALUES (?)");
         for (var i = 0; i < 10; i++) {
             stmt.run("Ipsum " + i);
         }
         stmt.finalize();
         
-        // db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+        // db.each("SELECT rowid AS id, info FROM order", function(err, row) {
         //     console.log(row.id + ": " + row.info);
         // });
     });
